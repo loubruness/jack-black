95bd5f43c01f662971bd56b56c6072a5dabf02a9
loubruness <94389521+loubruness@users.noreply.github.com>
2024-07-04 17:14:01 +0200

    singleton

--- card.py
+++ card.py
@@ -1,6 +1,16 @@
 import random
 
 class Deck:
+
+    # Singleton design pattern
+    _instance = None
+    
+    def __new__(cls, nb_decks=1):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.__init__(nb_decks)
+        return cls._instance
+    
     def __init__(self, nb_decks=1):
         self.nb_decks = nb_decks
         self.num_card = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
